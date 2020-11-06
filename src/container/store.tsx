2e2cbce945e06a5b0d6a3d6b2b9e0826c415c7bc
museumGoTeam <museumgo.team@gmail.com>
2020-11-06 21:40:47 +0100

    adding global store using context provider

---
+++ src/container/store.tsx
@@ -0,0 +1,37 @@
+import React, { createContext, PropsWithChildren, useContext, useReducer } from "react";
+import reducer from "./reducer";
+import { IAction, IAppState, TActionsType, TPayloadType } from "./types";
+
+const initialValue: IAppState = {
+  map: [],
+  originPosSelected: undefined,
+  poiPosSelected: undefined,
+  entitySelected: undefined,
+  isPoiFormOpen: false,
+  pois: [],
+};
+
+const context = createContext<{
+  appState: IAppState;
+  dispatch: React.Dispatch<IAction>;
+}>({ appState: initialValue, dispatch: () => null });
+
+export const useAppState = () => {
+  const { appState } = useContext(context);
+  return appState;
+};
+
+export const useDispatch = () => {
+  const { dispatch } = useContext(context);
+  return dispatch;
+};
+
+export const useStore = () => {
+  return useContext(context);
+};
+
+export default function StoreProvider({ children } : PropsWithChildren<any>) {
+    const [appState, dispatch] = useReducer(reducer, initialValue)
+
+    return <context.Provider value={{appState, dispatch}}>{children}</context.Provider>
+}
