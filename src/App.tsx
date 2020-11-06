4070d7a04a8a401f37bb933b78b3579e9fd55d3e
museumGoTeam <museumgo.team@gmail.com>
2020-11-06 21:45:36 +0100

    fetch the mp when App.tsx is mounted

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Panel from './components/Panel/Panel'
+import Canvas from './components/Canvas/Canvas'
+import { CANVAS_WIDTH, CANVAS_HEIGHT } from './constants'
+import { useGetMap } from './hooks/useMapReq'
 
 
 export default function App() {
+  const getMap = useGetMap()
+
+  React.useEffect(() => {
+    getMap()
+  }, [])
+
   return (
     <Grid container direction="column">
       <CssBaseline />
       <Panel />
+      <Canvas width={CANVAS_WIDTH} height={CANVAS_HEIGHT} />
     </Grid>
   )
 }
