f6cd5352b76de00263023acf6242bcb90c56e90a
museumGoTeam <museumgo.team@gmail.com>
2020-11-08 16:00:30 +0100

    allow admin to update a point of interest

--- src/components/PoiForm/PoiForm.tsx
+++ src/components/PoiForm/PoiForm.tsx
@@ -6,10 +6,11 @@ import Button from '../UI/Button'
 import ImageUploader from '../UI/ImageUploader'
 import Input from '../UI/Input'
 import InputArea from '../UI/InputArea'
-import {useLocation} from 'react-router-dom'
+import {useParams} from 'react-router-dom'
 import { IPOI } from '../Canvas/types'
-import classes from '*.module.css'
-import { IPoiForm } from './types'
+import axios from 'axios'
+import { APIRes } from '../../type'
+import { message } from 'antd'
 
 
 const useStyles = makeStyles(theme => ({
@@ -26,22 +27,49 @@ const useStyles = makeStyles(theme => ({
 
 export default function PoiForm() {
     const classes = useStyles()
-    const poi = useLocation<IPOI>().state
-    const [form, setForm] = React.useState<IPOI>(poi)
+    const routeParams = useParams<{id: string}>()
+    const [form, setForm] = React.useState<{loading: boolean, data: IPOI | undefined}>({
+        loading: true,
+        data: undefined
+    })
+
+
+    React.useEffect(() => {
+        const fetchPoi = async () => {
+            console.log(routeParams.id)
+            const fetchedPoi = (await axios.get<APIRes>(`/poi/${routeParams.id}`)).data
+            if (!fetchedPoi.success) {
+                message.error(fetchedPoi.message)
+                return
+            }
+            setForm({loading: false, data: fetchedPoi.data})
+        }
+        fetchPoi()
+    }, [routeParams.id])
 
     const handleChange = (name: string, value: File | string) => {
-        setForm({...form, [name]: value})
+        form && form.data && setForm({...form, data: {...form.data, [name]: value}})
     }
 
-    const onSubmit = () => {
-       
+    const onSubmit = async () => {
+       message.loading("The point of interest is updating ...")
+       const res = (await axios.put<APIRes>("/poi", form.data)).data
+       if (res.success) {
+           message.success(res.message)
+           return
+       }
+       message.error(res.message)
+
     }
+
+    if (form.loading) return <p>Loading ...</p>
+    if (!form.data) return <p>Error</p>
 
     return (
-        <Form title={`Details of the point of interest ${poi.name}`}>
-            <ImageUploader name="image" value={form.image as string} onUpload={(name, value) => handleChange(name, value)} />
-            <Input placeholder="name" value={form.name} onChange={e => handleChange("name", e.target.value)} marginVertical={8} />
-            <InputArea placeholder="description" value={form.description ? form.description : ""} onChange={e => handleChange("description", e.target.value)} marginVertical={8} />
+        <Form title={`Details of the point of interest ${form.data.name}`}>
+            <ImageUploader name="image" value={form.data.image as string} onUpload={(name, value) => handleChange(name, value)} />
+            <Input placeholder="name" value={form.data.name} onChange={e => handleChange("name", e.target.value)} marginVertical={8} />
+            <InputArea placeholder="description" value={form.data.description ? form.data.description : ""} onChange={e => handleChange("description", e.target.value)} marginVertical={8} />
             <Grid item container justify="center">
                 <Button label="Save"  onClick={onSubmit} color="primary" className={classes.button}  /> 
                 <Button label="Delete" className={`${classes.button} ${classes.buttonError}`} /> 
